8a1796ffc820babc27a76f33abef41f040cfd99d
khalid <khalid.rahmani.mail@gmail.com>
2015-05-07 21:32:20 +0100

    created organization

--- app/controllers/organization_controller.js
+++ app/controllers/organization_controller.js
@@ -14,48 +14,33 @@ exports.index = function (req, res){
 }
 
 exports.new = function (req, res){
-	res.render('organization/form', {   
+	res.render('organization/form', { 
 		organization: new Organization(),
 		label: 'New Organization',
-		action: "/organization/create"
+		action: '/organization/create'
 	})
 }
 
 exports.create = function (req, res){
 	var organization = new Organization(req.body)
-	var user         = new User(req.body)
-	organization.validate(function (err1) {
-		user.validate(function (err2) {
-	    if (err1 || err2) {
-	    err1 = 	(err1) ? err1.errors : {}
-	    err2 = 	(err2) ? err2.errors : {}
-	    errors = extend(err1, err2)	
+	organization.save(function (err) {
+		if(err){
 	      return res.render('organization/form',{
-	        errors: errors,
+	        errors: err.errors,
 	        organization: req.body,
 	        label: 'New Organization',
 	        action: "/organization/create"
 	      });
-	    }
-	    else {
-	    	user.role        = 'Customer_Admin'
-	    	user.setPassword()
-	    	user.organization = organization
-	    	user.save(function (err2) {
-		    	organization.admin = user
-		    	organization.save(function (err1) {
-		    		req.flash('message', {type: 'success', message: 'Organization created !'});   
-		        	res.send({status: "saved", url: "/organizations"})
-		        })  
-	    	})
-	     }  
+		}
+		else{
+    		req.flash('message', {type: 'success', message: 'Organization created !'});   
+        	res.send({status: "saved", url: "/organizations"})			
+		}
 	})
-  });
 }
 
 exports.edit = function (req, res){
-	Organization.findOne({ _id:  req.params.id}).populate('admin').exec(function (err, organization) {
-		console.log(organization)
+	Organization.findOne({ _id:  req.params.id}, function (err, organization) {		
 		res.render('organization/form', {
 			organization: organization,
 			label: 'Update Organization',
@@ -66,34 +51,23 @@ exports.edit = function (req, res){
 }
 
 exports.update = function (req, res){
-	Organization.findOne({ _id:  req.params.id}).populate('admin').exec(function (err, organization) {
+	Organization.findOne({ _id:  req.params.id}, function (err, organization) {
 		organization = extend(organization, req.body)
-		user         = extend(organization.admin, req.body)
-		organization.validate(function (err1) {
-			user.validate(function (err2) {
-		    if (err1 || err2) {
-		    err1 = 	(err1) ? err1.errors : {}
-		    err2 = 	(err2) ? err2.errors : {}
-		    errors = extend(err1, err2)	
+		organization.save(function (err) {
+			if(err){
 		      return res.render('organization/form',{
-		        errors: errors,
+		        errors: err.errors,
 		        organization: req.body,
-		        notNew: true,
-		        label: 'Update Organization',
-		        action: "/organization/"+organization.id+"/update"
+				label: 'Update Organization',
+				notNew: true,
+				action: "/organization/"+organization.id+"/update"
 		      });
-		    }
-		    else {
-		    	user.setPassword()
-		    	user.save(function (err2) {
-			    	organization.save(function (err1) {
-			    		req.flash('message', {type: 'success', message: 'Organization updated !'});   
-			        	res.send({status: "saved", url: "/organizations"})     
-			        })  
-		    	})
-		     }	    
+			}
+			else{
+	    		req.flash('message', {type: 'success', message: 'Organization updated !'});  
+	        	res.send({status: "saved", url: "/organizations"})			
+			}
 		})
-	  })
 	})	
 }
 
--- app/models/organization.js
+++ app/models/organization.js
@@ -3,10 +3,19 @@ var  mongoose         = require('mongoose')
     ,validator        = require('validator')
     ,validate         = require('mongoose-validator')
     ,uniqueValidator  = require('mongoose-unique-validator')
+    ,extend           = require('mongoose-validator').extend
+
+extend('isLessThan3CommaSeparatedEmails', function (val) {
+  var valid = true;
+  var emailsArray = val.split(",");
+  if (emailsArray.length > 3) valid = false;
+  emailsArray.forEach(function (email) { if (! validator.isEmail(email)) valid = false; });  
+  return valid;
+}, 'Invalid emails');
 
 var OrganizationSchema = new Schema({
   organization_name:    { type: String, required: "Company name can't be blank", unique: true },
-  admin:                { type: Schema.ObjectId, ref : 'User'}
+  admin_emails:         { type: String, required: "Admins emails can't be blank", validate: validate({validator: 'isLessThan3CommaSeparatedEmails'}) }
 })
 
 mongoose.model('Organization', OrganizationSchema)
